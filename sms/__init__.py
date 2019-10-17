d6808936a5abb9790ae007dfb94000780e36523c
chima <chimaataman@gmail.com>
2019-10-17 15:54:46 +0100

    add main page, reports page and error page

--- sms/__init__.py
+++ sms/__init__.py
@@ -1,11 +1,23 @@
 from kivy.uix.screenmanager import ScreenManager
 
 # Load the forms
-from sms.forms.personalinfo import PersonalInfo
+from sms.forms.error import Error
 from sms.forms.signin import SigninWindow
+from sms.forms.main_page import MainPage
+from sms.forms.personalinfo import PersonalInfo
+from sms.forms.page_reports import Reports
 
 sm = ScreenManager()
+sign_in = SigninWindow(name='signin')
+main_page = MainPage(name='main_page')
+personal_info = PersonalInfo(name='personal_info')
+page_reports = Reports(name='page_reports')
+error = Error(name='error')
 
 # Adds the screens in the order that they would appear
-sm.add_widget(SigninWindow(name='signin'))
-sm.add_widget(PersonalInfo(name='personalinfo'))
+sm.add_widget(sign_in)
+sm.add_widget(main_page)
+sm.add_widget(personal_info)
+sm.add_widget(page_reports)
+sm.add_widget(error)
+
---
+++ sms/forms/error.py
@@ -0,0 +1,31 @@
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
+
+try:
+    'Imported as a module'
+    import os
+    from sms.utils.dialog import OpenFileDialog
+except ImportError:
+    'Ran as a script'
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    from utils.dialog import OpenFileDialog
+
+form_root = os.path.dirname(__file__)
+kv_path = os.path.join(form_root, 'kv_container', 'error.kv')
+Builder.load_file(kv_path)
+
+# Startup configurations
+win_size = Window.size
+
+
+class Error(Screen):
+    form_root = form_root
+    win_size = win_size
+
+
+if __name__ == '__main__':
+    from kivy.app import runTouchApp
+
+    runTouchApp(Error())
