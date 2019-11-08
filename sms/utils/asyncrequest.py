15be55dd3b86989c7b7e4a31399804c91856b4e8
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2019-11-08 02:29:18 +0100

    Created asyncrequest.py

---
+++ sms/utils/asyncrequest.py
@@ -0,0 +1,69 @@
+'''
+AsyncRequest
+============
+
+Class for making ascynchronous request to the api server. The
+method arguement definies the type of http request to make
+
+	method = 'GET' - for querying the database
+	method = 'POST' - for adding new items to the database
+	method = 'PUT' - for updating a record in the database
+	method = 'DELETE' - for deleting a record from the database
+'''
+
+import requests
+from threading import Thread
+from sms.utils.popups import ErrorPopup
+
+
+class AsyncRequest(Thread):
+    def __init__(self, url, on_success=None, on_failure=None, headers=[],
+                 params=None, data=None, method='GET', **kwargs):
+        super(AsyncRequest, self).__init__(**kwargs)
+
+        self.url = url
+        self.on_success = on_success
+        self.on_failure = on_failure
+        self.params = params
+        self.data = data
+        self.method = method
+
+        self.headers = {
+            'Content-type': 'application/json', 'API_KEY': ''
+        } if not headers else headers
+
+        self.start()
+
+    def run(self):
+        try:
+            if self.method == 'GET':
+                self.resp = requests.get(
+                    self.url, headers=self.headers, params=self.params)
+            elif self.method == 'POST':
+                self.resp = requests.post(
+                    self.url, headers=self.headers, json=self.data)
+            elif self.method == 'PUT':
+                self.resp = requests.put(
+                    self.url, headers=self.headers, json=self.data)
+            elif self.method == 'DELETE':
+                self.resp = requests.delete(self.url, headers=self.headers)
+            else:
+                raise ValueError(
+                    '"method" arguement must be one of "GET", "POST", "PUT" or "DELETE"')
+        except requests.ConnectionError:
+            msg = 'Server down'
+            ErrorPopup(msg)
+            # Restart server
+        else:
+            status_code = self.resp.status_code // 100
+            if status_code in (1, 2):
+                if callable(self.on_success):
+                    self.on_success(self.resp)
+            elif status_code in (4, 5):
+                if callable(self.on_failure):
+                    self.on_failure(self.resp)
+                else:
+                    err_resp = self.resp.json()
+                    msg = r'[b]{}[/b]'.format(err_resp['title'].capitalize()
+                                              ) + ' : ' + err_resp['detail']
+                    ErrorPopup(msg)
