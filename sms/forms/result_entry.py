e89f267bedc8af414915c6e6b59ee8c8ca2d5d77
chima <chimaataman@gmail.com>
2020-03-19 05:12:25 +0100

    add results page

---
+++ sms/forms/result_entry.py
@@ -0,0 +1,76 @@
+import os
+import json
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
+from kivy.uix.popup import Popup
+from kivy.factory import Factory
+
+from sms import urlTo, get_api_key
+from sms.utils.asyncrequest import AsyncRequest
+from sms.utils.popups import ErrorPopup, SuccessPopup
+from sms.utils.dialog import OpenFileDialog
+
+form_root = os.path.dirname(__file__)
+kv_path = os.path.join(form_root, 'kv_container', 'result_entry.kv')
+Builder.load_file(kv_path)
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class Result_Entry(Screen):
+    form_root = form_root
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    #Replace save method with API Query or POST method
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
+
+Factory.register('Result_Entry', cls=Result_Entry)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
+
+
+if __name__ == '__main__':
+    from kivy.app import runTouchApp
+    from kivy.core.window import Window
+
+    Window.maximize()
+    runTouchApp(Result_Entry())
