4357bcb31519f7050e7191c12ff65617fde91418
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-04-26 14:55:31 +0100

    Base widget for all reports preview

---
+++ sms/forms/reports.py
@@ -0,0 +1,35 @@
+import os
+from threading import Thread
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+from sms.forms.template import FormTemplate
+from sms.forms.result_update import generate_result_update_preview
+from sms.utils.tabbedview import CustomTabbedPanelHeader
+
+form_root = os.path.dirname(__file__)
+kv_path = os.path.join(form_root, 'kv_container', 'reports.kv')
+Builder.load_file(kv_path)
+
+
+class Reports(FormTemplate):
+    tabbed_panel = ObjectProperty(None)
+
+    def generate_report(self, data, tab_title, report_type):
+        if report_type == 'result_update':
+            screens = generate_result_update_preview(data)
+        Thread(target=self.add_screens, args=(tab_title, screens)).start()
+
+    def add_screens(self, tab_title, screens):
+        tab_header = CustomTabbedPanelHeader(text=tab_title)
+        for screen in screens:
+            tab_header.add_screen(screen)
+        self.tabbed_panel.add_widget(tab_header)
+
+
+if __name__ == '__main__':
+    from kivy.app import runTouchApp
+    from kivy.core.window import Window
+
+    Window.maximize()
+    runTouchApp(Reports())
