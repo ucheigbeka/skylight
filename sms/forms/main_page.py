a64792befd6fec48c6e387a69e7514fdcf2a8825
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-04-29 14:28:30 +0100

    Added a view for the log

--- sms/forms/main_page.py
+++ sms/forms/main_page.py
@@ -1,17 +1,43 @@
 import os
+from time import localtime, asctime
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from kivy.clock import Clock
+from kivy.properties import ListProperty, ObjectProperty
+
+from sms import get_log
+from sms.forms.template import FormTemplate
 
 form_root = os.path.dirname(__file__)
 kv_path = os.path.join(form_root, 'kv_container', 'main_page.kv')
 Builder.load_file(kv_path)
 
 
-class MainPage(Screen):
-    form_root = form_root
+class MainPage(FormTemplate):
+    logs = ListProperty()
+    dv = ObjectProperty(None)
 
+    def __init__(self, **kwargs):
+        super(MainPage, self).__init__(**kwargs)
+        self.dv.dv.rv.viewclass = 'DataViewerLabel'
 
-if __name__ == '__main__':
-    from kivy.app import runTouchApp
+    def on_enter(self):
+        self.query_logs(0)
+        self.event = Clock.schedule_interval(self.query_logs, 30)
 
-    runTouchApp(MainPage())
+    def on_leave(self):
+        Clock.unschedule(self.event)
+        self.logs = [['', '', '']]
+
+    def query_logs(self, dt):
+        get_log(self.populate_dv, limit=10)
+
+    def populate_dv(self, resp):
+        logs = resp.json()
+        d_logs = []
+        for log in logs[::-1]:
+            timestamp, action = log
+            timestamp = asctime(localtime(timestamp))
+            if len(action) > 35:
+                action = action[:35] + '...'
+            d_logs.append([action[: action.find(' ')], action, timestamp])
+        self.logs = d_logs
