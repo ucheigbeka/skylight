8b5ab77319bcef5ce8604f3c894164d8c602c81d
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-25 23:04:49 +0100

    Changed the name of the base form from Reports to PageReports

--- sms/forms/page_reports.py
+++ sms/forms/page_reports.py
@@ -2,11 +2,13 @@ import os
 from kivy.lang import Builder
 
 from sms.forms.template import FormTemplate
+from sms.scripts.student_update import StudentUpdatePopup
 
 form_root = os.path.dirname(__file__)
 kv_path = os.path.join(form_root, 'kv_container', 'page_reports.kv')
 Builder.load_file(kv_path)
 
 
-class Reports(FormTemplate):
-    pass
+class PageReports(FormTemplate):
+    def open_student_update_popup(self, *args):
+        StudentUpdatePopup().open()
