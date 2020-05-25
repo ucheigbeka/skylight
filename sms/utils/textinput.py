9d741128e4797feda1c6691563ac605ed40ebe34
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-26 00:30:22 +0100

    Changed all tabs to spaces

--- sms/utils/textinput.py
+++ sms/utils/textinput.py
@@ -3,24 +3,24 @@ from kivy.uix.textinput import TextInput
 
 Builder.load_string('''
 <CustomTextInput>:
-	size_hint_x: None
-	width: 300
-	multiline: False
-	write_tab: False
-	use_bubble: True
+    size_hint_x: None
+    width: 300
+    multiline: False
+    write_tab: False
+    use_bubble: True
 ''')
 
 
 class CustomTextInput(TextInput):
-	def __init__(self, **kwargs):
-		super(CustomTextInput, self).__init__(**kwargs)
-		self.max_length = None
+    def __init__(self, **kwargs):
+        super(CustomTextInput, self).__init__(**kwargs)
+        self.max_length = None
 
-	def insert_text(self, substring, *args):
-		if self.max_length and (len(self.text) == self.max_length):
-			return
-		super(CustomTextInput, self).insert_text(substring, *args)
+    def insert_text(self, substring, *args):
+        if self.max_length and (len(self.text) == self.max_length):
+            return
+        super(CustomTextInput, self).insert_text(substring, *args)
 
-	def validate(self, instance):
-		if not instance.text:
-			instance.background_color = [1, 0, 0, 1]
+    def validate(self, instance):
+        if not instance.text:
+            instance.background_color = [1, 0, 0, 1]
