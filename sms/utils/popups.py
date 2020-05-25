01cd0139580149efef1968ff42c9f357d48fce83
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-26 00:22:12 +0100

    Created another content class for a persistent popup

--- sms/utils/popups.py
+++ sms/utils/popups.py
@@ -1,25 +1,47 @@
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, BooleanProperty
 
 Builder.load_string('''
 <PopupLabel>:
     text_size: self.size
     valign: 'middle'
     halign: 'center'
+
+<DismissablePopupLabel>:
+    orientation: 'vertical'
+    PopupLabel:
+        text: root.text
+        markup: root.markup
+    Button:
+        size_hint_x: .3
+        pos_hint: {'center_x': .5}
+        text: 'Close'
+        on_press: root.dismiss = True
 ''')
 
 
 class PopupLabel(Label):
-    def __init__(self, **kwargs):
-        super(PopupLabel, self).__init__(**kwargs)
+    pass
+
+
+class DismissablePopupLabel(BoxLayout):
+    text = StringProperty()
+    markup = BooleanProperty(True)
+    dismiss = BooleanProperty(False)
 
 
 class ErrorPopup(Popup):
     def __init__(self, message, **kwargs):
         super(ErrorPopup, self).__init__(**kwargs)
         self.title = kwargs.get('title', 'Error')
-        self.content = PopupLabel(text=message, markup=True)
+        if self.auto_dismiss:
+            self.content = PopupLabel(text=message, markup=True)
+        else:
+            self.content = DismissablePopupLabel(text=message, markup=True)
+            self.content.bind(dismiss=lambda ins, val: self.dismiss())
         self.size_hint = (.4, .2)
 
         self.open()
@@ -29,7 +51,11 @@ class SuccessPopup(Popup):
     def __init__(self, message, **kwargs):
         super(SuccessPopup, self).__init__(**kwargs)
         self.title = kwargs.get('title', 'Success')
-        self.content = PopupLabel(text=message, markup=True)
+        if self.auto_dismiss:
+            self.content = PopupLabel(text=message, markup=True)
+        else:
+            self.content = DismissablePopupLabel(text=message, markup=True)
+            self.content.bind(dismiss=lambda ins, val: self.dismiss())
         self.size_hint = (.4, .2)
 
         self.open()
