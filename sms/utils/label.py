e5acdb8449d187aae0722bc349002d1249d20add
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-26 00:18:43 +0100

    Editor changed all tabs to actual spaces

--- sms/utils/label.py
+++ sms/utils/label.py
@@ -3,12 +3,12 @@ from kivy.uix.label import Label
 
 Builder.load_string('''
 <CustomLabel>:
-	text_size: self.size
-	size_hint_x: None
-	width: 200
-	halign: 'center'
+    text_size: self.size
+    size_hint_x: None
+    width: 200
+    halign: 'center'
 ''')
 
 
 class CustomLabel(Label):
-	pass
+    pass
