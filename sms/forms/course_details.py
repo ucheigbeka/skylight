d9ff2712dc2a6acc369c1cd63fcdc85e31b9b681
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-25 22:44:38 +0100

    Added the options, start and end date fields

--- sms/forms/course_details.py
+++ sms/forms/course_details.py
@@ -1,5 +1,4 @@
 import os
-import json
 from kivy.lang import Builder
 
 from sms import urlTo
@@ -20,9 +19,11 @@ class CourseDetails(FormTemplate):
         AsyncRequest(url, params=params, on_success=self.populate_fields, on_failure=self.show_error)
 
     def populate_fields(self, resp):
-        data = json.loads(resp.json())
+        data = resp.json()[0]
         for field in self.ids.keys():
             self.ids[field].text = str(data[field])
+        self.ids['end_date'].text = '' if data['end_date'] == 2999 else str(data['end_date'])
+        self.ids['options'].text = ['Yes', 'No'][data['options'] == 0]
 
     def show_error(self, resp):
         ErrorPopup('Course not found')
