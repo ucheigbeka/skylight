5a5996aea41814f08f8bc5ac09c25d0b0c07358c
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-07-18 03:07:21 +0100

    Added callback for the gpa_cards view

--- sms/forms/page_reports.py
+++ sms/forms/page_reports.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 
 from sms.forms.template import FormTemplate
 from sms.scripts.student_update import StudentUpdatePopup
+from sms.scripts.gpa_cards import GpaCardsPopup
 
 form_root = os.path.dirname(__file__)
 kv_path = os.path.join(form_root, 'kv_container', 'page_reports.kv')
@@ -10,5 +11,8 @@ Builder.load_file(kv_path)
 
 
 class PageReports(FormTemplate):
-    def open_student_update_popup(self, *args):
+    def open_student_update_popup(self):
         StudentUpdatePopup().open()
+
+    def open_gpa_cards_popup(self):
+        GpaCardsPopup().open()
