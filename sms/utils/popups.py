3910649deec16ccaa7a5a27896f3028084309b11
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-08-02 00:35:42 +0100

    Restructured this module and created a new type of popup

--- sms/utils/popups.py
+++ sms/utils/popups.py
@@ -9,17 +9,32 @@ Builder.load_string('''
     text_size: self.size
     valign: 'middle'
     halign: 'center'
+    markup: True
 
 <DismissablePopupLabel>:
     orientation: 'vertical'
     PopupLabel:
         text: root.text
-        markup: root.markup
+        markup: True
     Button:
         size_hint_x: .3
         pos_hint: {'center_x': .5}
         text: 'Close'
         on_press: root.dismiss = True
+
+<YesNoPopupContent>:
+    orientation: 'vertical'
+    PopupLabel:
+        text: root.text
+        markup: True
+    BoxLayout:
+        size_hint_x: .3
+        Button:
+            text: 'Yes'
+            on_press: root.yes()
+        Button:
+            text: 'No'
+            on_press: root.no()
 ''')
 
 
@@ -29,33 +44,53 @@ class PopupLabel(Label):
 
 class DismissablePopupLabel(BoxLayout):
     text = StringProperty()
-    markup = BooleanProperty(True)
     dismiss = BooleanProperty(False)
 
 
-class ErrorPopup(Popup):
+class YesNoPopupContent(BoxLayout):
+    text = StringProperty()
+
+    def yes(self):
+        pass
+
+    def no(self):
+        pass
+
+    def set_callbacks(self, yes, no):
+        self.yes = yes
+        self.no = no
+
+
+class PopupBase(Popup):
+    def __init__(self, message=None, **kwargs):
+        super(PopupBase, self).__init__(**kwargs)
+        if not self.content:
+            if self.auto_dismiss:
+                self.content = PopupLabel(text=message)
+            else:
+                self.content = DismissablePopupLabel(text=message)
+                self.content.bind(dismiss=lambda ins, val: self.dismiss())
+            self.size_hint = (.4, .2)
+
+        self.open()
+
+
+class ErrorPopup(PopupBase):
     def __init__(self, message, **kwargs):
-        super(ErrorPopup, self).__init__(**kwargs)
         self.title = kwargs.get('title', 'Error')
-        if self.auto_dismiss:
-            self.content = PopupLabel(text=message, markup=True)
-        else:
-            self.content = DismissablePopupLabel(text=message, markup=True)
-            self.content.bind(dismiss=lambda ins, val: self.dismiss())
-        self.size_hint = (.4, .2)
-
-        self.open()
+        super(ErrorPopup, self).__init__(message, **kwargs)
 
 
-class SuccessPopup(Popup):
+class SuccessPopup(PopupBase):
     def __init__(self, message, **kwargs):
-        super(SuccessPopup, self).__init__(**kwargs)
         self.title = kwargs.get('title', 'Success')
-        if self.auto_dismiss:
-            self.content = PopupLabel(text=message, markup=True)
-        else:
-            self.content = DismissablePopupLabel(text=message, markup=True)
-            self.content.bind(dismiss=lambda ins, val: self.dismiss())
-        self.size_hint = (.4, .2)
+        super(SuccessPopup, self).__init__(message, **kwargs)
 
-        self.open()
+
+class YesNoPopup(PopupBase):
+    def __init__(self, message, on_yes=None, on_no=None, **kwargs):
+        self.title = kwargs.get('title', 'Info')
+        self.content = YesNoPopupContent(text=message)
+        self.content.set_callbacks(on_yes, lambda: on_no())
+        self.auto_dismiss = False
+        super(YesNoPopup, self).__init__(**kwargs)
