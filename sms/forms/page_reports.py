80beba772bbb6355eeef864bfe374d30fd7ab1ce
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-08-02 00:29:03 +0100

    Minor changes to support multiple .kv files based on the user's title

--- sms/forms/page_reports.py
+++ sms/forms/page_reports.py
@@ -1,12 +1,11 @@
-import os
 from kivy.lang import Builder
 
+from sms import get_kv_path
 from sms.forms.template import FormTemplate
 from sms.scripts.student_update import StudentUpdatePopup
 from sms.scripts.gpa_cards import GpaCardsPopup
 
-form_root = os.path.dirname(__file__)
-kv_path = os.path.join(form_root, 'kv_container', 'page_reports.kv')
+kv_path = get_kv_path('page_reports')
 Builder.load_file(kv_path)
 
 
