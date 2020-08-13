dd5dac6cb897fa6f53968957b777178fa14323fa
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-08-13 03:16:59 +0100

    Added logic to dynamically load the kv rule based on the user's details

--- sms/forms/admin.py
+++ sms/forms/admin.py
@@ -1,11 +1,9 @@
-import os
 from kivy.lang import Builder
 
+from sms import get_kv_path
 from sms.forms.template import FormTemplate
 
-
-form_root = os.path.dirname(__file__)
-kv_path = os.path.join(form_root, 'kv_container', 'admin.kv')
+kv_path = get_kv_path('admin')
 Builder.load_file(kv_path)
 
 
