5dbe25542591fcb1990e5c8f344aeb578422e45e
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-14 01:50:46 +0100

    Major changes

--- sms/forms/result_entry.py
+++ sms/forms/result_entry.py
@@ -1,159 +1,123 @@
 import os
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
 
-from sms import urlTo
+from sms import urlTo, get_current_session, get_assigned_level
 from sms.forms.template import FormTemplate
+from sms.forms.result_entry_single import insert_extra
 from sms.utils.asyncrequest import AsyncRequest
 from sms.utils.popups import ErrorPopup
-from sms.utils.dialog import OpenFileDialog
-
-from xlrd import open_workbook, XL_CELL_NUMBER
 
 form_root = os.path.dirname(__file__)
 kv_path = os.path.join(form_root, 'kv_container', 'result_entry.kv')
 Builder.load_file(kv_path)
 
-HEADER = ['COURSE_CODE', 'SESSION', 'MATNO', 'SCORE']
+DEFAULT_DV_DATA = [
+    ['Completed', ''],
+    ['Outstanding', ''],
+    ['No. of students', '']
+]
 
 
-def parse_xl_sheet(sheet):
-    data = []
-    if sheet.ncols != 4:
-        raise ValueError('err')
-    header = list(map(lambda x: x.upper(), sheet.row_values(0)))
-    sequence = []
-    for val in header:
-        sequence.append(HEADER.index(val))
-    for row_idx in range(1, sheet.nrows):
-        row = [0] * 4
-        for col_idx in range(sheet.ncols):
-            index = sequence[col_idx]
-            cell = sheet.cell(row_idx, col_idx)
-            if cell.ctype == XL_CELL_NUMBER:
-                row[index] = int(cell.value)
-            else:
-                row[index] = str(cell.value)
-        data.append(row)
-
-    return data
-
-
-class LoadPopupContent(BoxLayout):
-    pass
-
-
-class LoadPopup(Popup):
-    def __init__(self, **kwargs):
-        super(LoadPopup, self).__init__(**kwargs)
-        self.title = 'Load file'
-        self.content = LoadPopupContent()
-        self.size_hint = (.2, .18)
-
-        self.open()
-
-
-class Result_Entry(FormTemplate):
-    edv = ObjectProperty(None)
-
+class ResultEntry(FormTemplate):
     title = 'Result Entry'
+    dv = ObjectProperty()
+    dv2 = ObjectProperty()
 
     def __init__(self, **kwargs):
-        super(Result_Entry, self).__init__(**kwargs)
-        pass
+        super(ResultEntry, self).__init__(**kwargs)
+        self.dv2.dv.set_viewclass('DataViewerLabel')
+        self.dv2.dv.bind(selected_indexes=self.open_in_singles_screen)
 
-    def strip_data(self):
-        text = self.text_input.text.strip().split('\n')
-        data = []
-        for row in text:
-            res = row.strip().split()
-            if len(res) != 4:
-                return None
-            data.append(res)
-        return data
+    def on_enter(self, *args):
+        self.persist_data = False
 
-    def dismiss_popup(self):
-        self._popup.dismiss()
+    def setup(self):
+        self.data = []
+        self.query_params = {}
+        self.selected_indexes = []
+        self.ids['session'].text = str(get_current_session())
+        assigned_level = get_assigned_level()
+        self.ids['level'].text = '' if not assigned_level else str(assigned_level)
+        self.dv2.dv._data = [[''] * self.dv2.cols]
+        self.dv.set_data(DEFAULT_DV_DATA)
 
-    def show_load(self):
-        filter_dict = {
-            'Excel Files (*.xls, *.xlsx)': ['*.xls', '*.xlsx'],
-            'Text Document (*.txt)': ['*.txt'], 'All Files': ['*']
-        }
-        dialog = OpenFileDialog(filter_dict=filter_dict)
-        dialog.bind(on_dismiss=self.update_dataview)
-        dialog.open()
-
-    def parse_txt(self, instance):
-        try:
-            str_list = instance.load_file().split('\n')
-            data, str_len = [], len(str_list)
-            for idx in range(str_len):
-                row = str_list[idx]
-                _row = row.split('\t')
-                if len(_row) != 4 and idx != str_len - 1:
-                    raise ValueError
-                data.append(_row)
-            if data[-1] == ['']:
-                data.pop()
-            if len(self.edv.data) == 1 and self.edv.data[0] == [''] * 4:
-                self.edv.data = data
-            else:
-                self.edv.data.extend(data)
-        except ValueError:
-            ErrorPopup('Error parsing {}'.format(instance.file_name))
-
-    def parse_xl(self, filepath):
-        try:
-            xl_workbook = open_workbook(filepath)
-            data = []
-            for sheet in xl_workbook.sheets():
-                data.extend(parse_xl_sheet(sheet))
-            if len(self.edv.data) == 1 and self.edv.data[0] == [''] * 4:
-                self.edv.data = data
-            else:
-                self.edv.data.extend(data)
-        except ValueError:
-            ErrorPopup('Error parsing {}'.format(os.path.basename(filepath)))
-
-    def update_dataview(self, instance):
-        if instance.selected_path:
-            ext = os.path.splitext(instance.selected_path)[1]
-            if ext in ['.xls', '.xlsx']:
-                self.parse_xl(instance.selected_path)
-            else:
-                self.parse_txt(instance)
-
-    def clear_dataview(self, resp):
-        resp = resp.json()
-        if resp:
-            err_msg = '\n'.join(resp)
-            ErrorPopup(err_msg, title='Alert')
-        self.edv.data = [[''] * 4]
-        # Refreshs the dataview
-        self.edv.data.append([''] * 4)
-        self.edv.data = [[''] * 4]
-
-    def validate_data(self, data):
-        if not data:
-            return '', False
-        for idx in range(len(data)):
-            if not data[idx][1].isnumeric() or not data[idx][3].isnumeric():
-                return idx, False
-        return '', True
-
-    def upload(self, *args):
-        url = urlTo('results')
-        dv = self.edv.get_dataviewer()
-        data = dv.get_data()
-        idx, is_valid = self.validate_data(data)
-        if is_valid:
-            AsyncRequest(url, data=data, method='POST',
-                         on_success=self.clear_dataview)
+    def search(self):
+        url = urlTo('results_stats_multiple')
+        if self.validate_inputs():
+            session = int(self.ids['session'].text)
+            level = int(self.ids['level'].text)
+            self.query_params = {'acad_session': session, 'level': level}
+            AsyncRequest(url, params=self.query_params, on_success=self.populate_fields)
         else:
-            if idx != '':
-                ErrorPopup('Error parsing results at index ' + str(idx))
+            ErrorPopup('Fields can\'t be empty')
+
+    def query_results_stats(self, mat_no):
+        url = urlTo('results_stats_single')
+        params = {'mat_no': mat_no}
+        params.update(self.query_params)
+        AsyncRequest(url, params=params, on_success=self.refresh_fields)
+
+    def refresh(self):
+        self.num_queued_queries = 0
+        for idx in self.selected_indexes:
+            mat_no = self.data[idx][0]
+            self.query_results_stats(mat_no)
+            self.num_queued_queries += 1
+
+    def refresh_fields(self, resp):
+        data = resp.json()
+        index = self.selected_indexes.pop(0)
+        self.data[index] = data
+        self.num_queued_queries -= 1
+
+        if not self.num_queued_queries:
+            self.dv2.dv.set_data(self.data)
+            self.populate_stats_fields()
+
+    def compute_stats(self, data):
+        num_completed = 0
+        num_outsanding = 0
+        num_students = 0
+        for course_details in data:
+            if course_details[2] and course_details[2] == course_details[3]:
+                num_completed += 1
             else:
-                ErrorPopup('Error parsing results. Check your input')
+                num_outsanding += 1
+            num_students += 1
+        return num_completed, num_outsanding, num_students
+
+    def populate_fields(self, resp):
+        data = resp.json()
+        if data:
+            self.data = data
+            self.dv2.dv.set_data(data)
+            self.populate_stats_fields()
+        else:
+            self.dv2._data = [[''] * self.dv2.cols]
+
+    def populate_stats_fields(self):
+        stats = self.compute_stats(self.data)
+        stats_data = [row[:] for row in DEFAULT_DV_DATA]
+        for idx in range(len(stats_data)):
+            stats_data[idx][1] = str(stats[idx])
+        self.dv.set_data(stats_data)
+
+    def open_in_singles_screen(self, instance, value):
+        if value:
+            data = self.dv2.dv.get_selected_items()[0]
+            selected_indexes = self.dv2.dv.selected_indexes[:]
+            self.selected_indexes.extend(selected_indexes)
+            params = {
+                'mat_no': data[0],
+                'acad_session': int(self.ids['session'].text)
+            }
+            insert_extra(params)
+            self.persist_data = True
+            self.manager.transition.direction = 'left'
+            self.manager.current = 'result_entry_single'
+            self.dv2.dv.deselect(value[0])
+
+    def clear_fields(self):
+        if not self.persist_data:
+            super(ResultEntry, self).clear_fields()
