8f265a841924d6961e19a48ee30f260940d41e21
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-19 01:59:18 +0100

    Created script for logging out

---
+++ sms/scripts/logout.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+
+from sms import urlTo, get_token, set_details
+from sms.forms.signin import SigninWindow
+from sms.utils.menubar import LoginActionView
+from sms.utils.asyncrequest import AsyncRequest
+
+
+def logout():
+    url = urlTo('logout')
+    AsyncRequest(url, method='POST', data={'token': get_token()}, on_success=reset)
+
+
+def reset(resp):
+    root = App.get_running_app().root
+    sm = root.sm
+    set_details('', '', '')
+    root.title = ''
+    root.is_admin = False
+    root.assigned_level = 0
+
+    curr_screen = sm.current_screen
+    screens = sm.screens[:]
+    for screen in screens:
+        if screen == curr_screen or isinstance(screen, SigninWindow):
+            continue
+        sm.remove_widget(screen)
+
+    for module in root.sm.imported_modules.values():
+        if hasattr(module, 'unload') and not hasattr(module, 'SigninWindow'):
+            module.unload()
+
+    sm.transition.direction = 'right'
+    sm.current = 'signin'
+    root.set_menu_view(LoginActionView)
+    sm.remove_widget(curr_screen)
+    del screens, curr_screen
+    sm.forms_dict = {}
