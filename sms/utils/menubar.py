0ae137c2cecd1d9bf54072cf6138bbd9e1bf77d9
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-20 17:45:26 +0100

    Started working on the notification funtionality

--- sms/utils/menubar.py
+++ sms/utils/menubar.py
@@ -1,13 +1,19 @@
 import os
 from kivy.lang import Builder
+from kivy.graphics import Color, Ellipse
+from kivy.core.text import Label as CoreLabel
 from kivy.uix.actionbar import ActionBar, ActionView
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty,\
+    ObjectProperty
 
 Builder.load_string('''
 #:import os os
 
 <Separator@Label+ActionItem>:
     size_hint_x: .7
+
+<TestLabel@Label>:
+    text: '1'
 
 <LoginActionView>:
     spacing: dp(20)
@@ -21,6 +27,7 @@ Builder.load_string('''
         on_press: root.dispatch('on_exit_btn_pressed')
 
 <MainActionView>:
+    notification: notification
     spacing: dp(20)
     ActionPrevious:
         title: root.title
@@ -45,6 +52,7 @@ Builder.load_string('''
     Separator:
         important: True
     ActionButton:
+        id: notification
         text: 'Notifications'
         icon: os.path.join(root.base_dir, 'icons', 'icons8-bell-32.png')
         on_press: root.dispatch('on_notification_btn_pressed')
@@ -78,13 +86,24 @@ class LoginActionView(ActionView):
 
 
 class MainActionView(ActionView):
+    SUCCESS_BG_COLOR = (0, 1, 0, 1)
+    ERROR_BG_COLOR = (1, 0, 0, 1)
+    TEXT_COLOR = (0, 0, 0, 1)
+
+    notification = ObjectProperty()
+
     title = StringProperty()
     with_previous = BooleanProperty(True)
+    text_texture = ObjectProperty()
+    num_notifications = NumericProperty(0)
 
     base_dir = os.path.dirname(__file__)
 
     def __init__(self, **kwargs):
         super(MainActionView, self).__init__(**kwargs)
+
+        self.circular_bg = None
+        self.notification.bind(pos=self.update_drawing, size=self.update_drawing)
 
         self.register_event_type('on_previous_btn_pressed')
         self.register_event_type('on_home_btn_pressed')
@@ -93,6 +112,50 @@ class MainActionView(ActionView):
         self.register_event_type('on_notification_btn_pressed')
         self.register_event_type('on_profile_btn_pressed')
         self.register_event_type('on_logout_btn_pressed')
+
+    def compute_pos(self, instance):
+        pos_x = instance.pos[0] + instance.width - 20
+        pos_y = instance.pos[1] + (instance.height * .5)
+        return (pos_x, pos_y)
+
+    def compute_text_pos(self, pos):
+        return (pos[0] + 2.5), (pos[1] + 2.5)
+
+    def on_num_notifications(self, instance, value):
+        if value:
+            self.draw()
+        else:
+            self.clear_drawing()
+
+    def draw(self):
+        with self.notification.canvas:
+            Color(*self.SUCCESS_BG_COLOR)
+            pos = self.compute_pos(self.notification)
+            self.circular_bg = Ellipse(segments=500, size=(20, 20), pos=pos)
+            Color(*self.TEXT_COLOR)
+            self.not_text = Ellipse(size=(15, 15), pos=self.compute_text_pos(pos))
+            label = CoreLabel(text=str(self.num_notifications))
+            label.refresh()
+            self.not_text.texture = label.texture
+
+    def clear_drawing(self):
+        self.circular_bg.size = (0, 0)
+        self.not_text.size = (0, 0)
+
+    def update_drawing(self, instance, value):
+        if self.circular_bg:
+            pos = self.compute_pos(instance)
+            self.circular_bg.pos = pos
+            self.not_text.pos = self.compute_text_pos(pos)
+
+    def add_notification(self):
+        self.num_notifications += 1
+
+    def remove_notification(self):
+        self.num_notifications -= 1
+
+    def clear_notification(self):
+        self.num_notifications = 0
 
     def on_previous_btn_pressed(ins):
         pass
@@ -122,14 +185,16 @@ class MenuBar(ActionBar):
 
 if __name__ == '__main__':
     from kivy.app import runTouchApp
-
-    runTouchApp(
-        Builder.load_string('''
+    root = Builder.load_string('''
 BoxLayout:
     orientation: 'vertical'
     MenuBar:
+        id: bar
         size_hint_y: .1
-        LoginActionView
     BoxLayout
             ''')
-    )
+
+    main_action_view = MainActionView()
+    root.ids['bar'].add_widget(main_action_view)
+
+    runTouchApp(root)
