02431ef1a2c5b73e9325320a66aaac6aa3efc9f3
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-10-22 01:10:09 +0100

    Added the logic for opening the popup for the broadsheet and senate version

--- sms/forms/page_reports.py
+++ sms/forms/page_reports.py
@@ -2,7 +2,9 @@ from kivy.lang import Builder
 
 from sms import get_kv_path
 from sms.forms.template import FormTemplate
+from sms.scripts.broadsheet import BroadsheetPopup
 from sms.scripts.student_update import StudentUpdatePopup
+from sms.scripts.senate_version import SenateVersionPopup
 from sms.scripts.gpa_cards import GpaCardsPopup
 
 kv_path = get_kv_path('page_reports')
@@ -16,8 +18,14 @@ def unload():
 class PageReports(FormTemplate):
     title = 'Page Reports'
 
+    def open_broadsheet_popup(self):
+        BroadsheetPopup().open()
+
     def open_student_update_popup(self):
         StudentUpdatePopup().open()
+
+    def open_senate_version_popup(self):
+        SenateVersionPopup().open()
 
     def open_gpa_cards_popup(self):
         GpaCardsPopup().open()
