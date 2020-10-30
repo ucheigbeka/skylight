914711ed2ea9c63409fef4b8ac81775238ba0f0c
chima <chimaataman@gmail.com>
2020-10-30 04:45:16 +0100

    add set_resultedit feature to admin page

--- sms/forms/admin.py
+++ sms/forms/admin.py
@@ -1,7 +1,8 @@
 from kivy.lang import Builder
 
-from sms import get_kv_path
+from sms import get_kv_path, urlTo, AsyncRequest
 from sms.forms.template import FormTemplate
+from sms.utils.popups import ErrorPopup, SuccessPopup
 
 kv_path = get_kv_path('admin')
 Builder.load_file(kv_path)
@@ -13,3 +14,22 @@ def unload():
 
 class Administrator(FormTemplate):
     title = 'Administrator'
+
+    def set_results_edit(self):
+        switch = self.ids['result_switch']
+        url = urlTo('results_edit')
+        data = {'state': switch.active}
+        AsyncRequest(url, method='POST', data=data, on_success=self.sing_for_me, on_failure=self.show_error)
+
+    def sing_for_me(self, resp):
+        state = resp.json()
+        msg = 'Result Edit {}'.format('opened' if state else 'closed')
+        SuccessPopup(message=msg)
+
+    def show_error(self, resp):
+        try:
+            msg = resp.json()
+        except:
+            msg = 'Something went wrong'
+        msg = msg['details'] if 'details' in msg else msg
+        ErrorPopup(msg)
