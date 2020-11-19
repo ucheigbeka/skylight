ee67bdd9fe3fe95ec7150a062b94f41ce0df27ed
chima <chimaataman@gmail.com>
2020-11-19 20:23:08 +0100

    add feedback icon

--- sms/scripts/about.py
+++ sms/scripts/about.py
@@ -3,5 +3,9 @@ from sms.utils.popups import SuccessPopup
 
 def about():
     msg = ''' Student Management System
+    
+    Feedback Icon made by Those Icons from www.flaticon.com"
     '''
-    SuccessPopup(msg, title='Project Skylight', auto_dismiss=False)
+    about_popup = SuccessPopup(msg, title='Project Skylight', auto_dismiss=False)
+    about_popup.size_hint = (.4, .27)
+    about_popup.open()
