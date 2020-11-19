c6b8547281776f6b9f89c70eb6edde071fad5b31
chima <chimaataman@gmail.com>
2020-11-19 20:38:40 +0100

    refactor main.py, deprecate run.py

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import sys
 import traceback
 
-with open('logs.txt', 'w') as fd:
-    sys.stderr = fd
 
-    try:
-        from run import StudentManagementSystemApp
-        StudentManagementSystemApp().run()
-    except Exception:
-        traceback.print_exc(file=fd)
+def start():
+    import kivy
+    kivy.require('1.7.0')
+
+    from kivy.config import Config
+
+    Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+    Config.set('kivy', 'exit_on_escape', '0')
+
+    from kivy.app import App
+    from sms import root
+
+    class StudentManagementSystemApp(App):
+        title = 'Student Management System'
+        icon = 'skylight.png'
+
+        def build(self):
+            return root
+
+    StudentManagementSystemApp().run()
+
+
+try:
+    prev_log = open('logs.txt').read()
+except FileNotFoundError:
+    prev_log = ''
+
+fd = open('logs.txt', 'r+', buffering=1)
+
+if __name__ == '__main__':
+    with fd:
+        sys.stderr = fd
+        try:
+            start()
+        except Exception:
+            traceback.print_exc(file=fd)
+
