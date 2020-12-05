ff20e23c0166596f19e61ba0a2078aa0c79d4867
chima <chimaataman@gmail.com>
2020-12-05 04:52:49 +0100

    run sms_restarter.bat from TEMP_DIR
    this is to hide the script from the users

--- sms/scripts/updater.py
+++ sms/scripts/updater.py
@@ -4,16 +4,16 @@ import subprocess
 from zipfile import ZipFile
 
 from sms import urlTo, AsyncRequest
-from sms.setup import PROJECT_ROOT
+from sms.setup import PROJECT_ROOT, TEMP_DIR
 from sms.utils.popups import ErrorPopup, YesNoPopup
 
 
 def download_upgrade():
     url = urlTo('download_fe')
-    AsyncRequest(url, on_success=upgrade, on_failure=show_error)
+    AsyncRequest(url, on_success=extract, on_failure=show_error)
 
 
-def upgrade(resp):
+def extract(resp):
     filepath = os.path.join(os.path.expanduser('~'), 'sms.zip')
     if os.path.exists(filepath): os.unlink(filepath)
     output_dir = os.path.join(os.path.expanduser('~'), 'sms_temp')
@@ -28,12 +28,18 @@ def upgrade(resp):
     except Exception as e:
         show_error()
         return
+
+    # copy server config
+    config_path = os.path.join(PROJECT_ROOT, 'config.json')
+    if os.path.exists(config_path):
+        shutil.copyfile(config_path, os.path.join(output_dir, 'config.json'))
+
     YesNoPopup('Click "Yes" to restart with the new version', on_yes=restart, title='Updater')
 
 
 def restart():
     restarter_path = os.path.join(PROJECT_ROOT, 'restarter.bat')
-    temp_restarter_path = os.path.join(os.path.expanduser('~'), 'sms_restarter.bat')
+    temp_restarter_path = os.path.join(TEMP_DIR, 'sms_restarter.bat')
     shutil.rmtree(temp_restarter_path, ignore_errors=True)
     shutil.copyfile(restarter_path, temp_restarter_path)
     updater_logs_path = os.path.join(os.path.expanduser('~'), 'sms_updater_logs.txt')
