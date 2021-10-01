9be242e8a1aa903d2ace654dfc412f9c09b7e284
cAtaman <chimaataman@gmail.com>
2021-10-01 21:49:46 +0100

    rewrite personalinfo for compliance with API v3

--- sms/forms/personalinfo.py
+++ sms/forms/personalinfo.py
@@ -3,10 +3,10 @@ import json
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-from sms import urlTo, get_assigned_level, root
+from sms import urlTo, get_assigned_level, root, get_current_session
 from sms.forms.template import FormTemplate
 from sms.utils.asyncrequest import AsyncRequest
-from sms.utils.popups import ErrorPopup, SuccessPopup
+from sms.utils.popups import ErrorPopup, SuccessPopup, YesNoPopup
 from sms.utils.dialog import OpenFileDialog
 
 form_root = os.path.dirname(__file__)
@@ -32,6 +32,7 @@ class PersonalInfo(FormTemplate):
 
     def setup(self):
         self.ids.mat_no.text = 'ENG'
+        self.ids.session_admit.text = str(get_current_session())
         self.ids.passport.source = self.profile_icon
         self.ids['dob'].text = ''
         self.ids['btn_positive'].text = 'Add'
@@ -56,36 +57,6 @@ class PersonalInfo(FormTemplate):
             self.ids['mat_no'].text = EXTRAS.get('mat_no')
             self.search()
 
-    def add_update(self):
-        method = ['POST', 'PATCH'][self.ids['btn_positive'].text == 'Update']
-        data = dict()
-        data['mat_no'] = self.ids.mat_no.text
-        data['surname'] = self.ids.surname.text.upper()
-        data['othernames'] = ' '.join([self.ids.fname.text.capitalize(), self.ids.mname.text.capitalize()])
-        try:
-            data['mode_of_entry'] = MODES_OF_ENTRY.index(self.ids.mode_of_entry.text) + 1
-        except ValueError:
-            self.show_input_error()
-            return
-        data['session_admitted'] = int(self.ids.session_admit.text)
-        data['level'] = int(self.ids.level.text)
-        data['transfer'] = int(self.ids.transfer.text == 'Yes')
-        data['sex'] = ['M', 'F'][self.ids.sex.text != 'Male']
-        data['date_of_birth'] = self.ids.dob.text
-        data['state_of_origin'] = self.ids.state_of_origin.text
-        data['lga'] = self.ids.lga_of_origin.text
-        data['phone_no'] = self.ids.phone_no.text
-        data['email_address'] = self.ids.email.text
-        data['sponsor_phone_no'] = self.ids.s_phone_no.text
-        data['sponsor_email_address'] = self.ids.s_email.text
-        data['grad_status'] = int(self.ids.grad.text == 'YES')
-
-        url = urlTo('personal_info')
-        AsyncRequest(url, method=method, data=data, on_success=self.record_added, on_failure=self.show_add_error)
-
-    def delete(self):
-        pass
-
     def search(self, *args):
         url = urlTo('personal_info')
         param = {'mat_no': self.ids.mat_no.text}
@@ -94,10 +65,7 @@ class PersonalInfo(FormTemplate):
     def populate_fields(self, resp):
         data = json.loads(resp.json())
         self.ids.surname.text = data['surname']
-        if len(data['othernames'].split()) == 2:
-            self.ids.fname.text, self.ids.mname.text = data['othernames'].split()
-        else:
-            self.ids.fname.text, self.ids.mname.text = str(data['othernames']), ''
+        self.ids.fname.text, _, self.ids.mname.text = data['othernames'].partition(' ')
         self.ids.mode_of_entry.text = MODES_OF_ENTRY[data['mode_of_entry'] - 1]
         self.ids.session_admit.text = str(data['session_admitted'])
         self.ids.level.text = str(data['level'])
@@ -111,17 +79,60 @@ class PersonalInfo(FormTemplate):
         self.ids.s_phone_no.text = str(data['sponsor_phone_no']) if data['sponsor_phone_no'] else ''
         self.ids.s_email.text = str(data['sponsor_email_address']) if data['sponsor_email_address'] else ''
         self.ids.grad.text = self.ids.grad.values[not bool(data['grad_status'])]
+        self.ids.session_grad.text = str(data['session_grad']) if data['session_grad'] else ''
 
         self.ids['btn_positive'].text = 'Update'
         self.ids['btn_positive'].disabled = False if data['level'] == get_assigned_level() or root.sm.is_admin else True
         self.ids['delete'].disabled = False if data['level'] == get_assigned_level() or root.sm.is_admin else True
 
-    def show_error(self, resp):
-        if resp.status_code == 500:
-            ErrorPopup('Something went wrong')
+    def add_update(self):
+        method = ['POST', 'PATCH'][self.ids['btn_positive'].text == 'Update']
+        data = dict()
+        data['mat_no'] = self.ids.mat_no.text
+        data['surname'] = self.ids.surname.text.upper()
+        data['othernames'] = ' '.join([self.ids.fname.text.capitalize(), self.ids.mname.text.capitalize()])
+        try:
+            data['mode_of_entry'] = MODES_OF_ENTRY.index(self.ids.mode_of_entry.text) + 1
+        except ValueError:
+            self.show_input_error()
             return
-        resp = '' if not resp.text else resp.json()
-        ErrorPopup('Record not found: ' + resp)
+        data['session_admitted'] = int(self.ids.session_admit.text)
+        data['level'] = int(self.ids.level.text)
+        data['sex'] = ['M', 'F'][self.ids.sex.text != 'Male']
+        data['phone_no'] = self.ids.phone_no.text
+        data['email_address'] = self.ids.email.text
+
+        # not required
+        optionals = {
+             'lga': self.ids.lga_of_origin.text,
+             'date_of_birth': self.ids.dob.text,
+             'state_of_origin': self.ids.state_of_origin.text,
+             'sponsor_phone_no': self.ids.s_phone_no.text,
+             'sponsor_email_address': self.ids.s_email.text,
+             'transfer': int(self.ids.transfer.text == 'Yes'),
+             'session_grad': int(self.ids.session_grad.text or 0),
+             'grad_status': int(self.ids.grad.text == 'YES')
+        }
+        [data.update({k:v}) for (k,v) in optionals.items() if v is not None]
+        url = urlTo('personal_info')
+        AsyncRequest(url, method=method, params=data, on_success=self.record_added, on_failure=self.show_add_error)
+
+    def delete(self):
+        YesNoPopup(message="Do you want to delete this student's record?", on_yes=self._delete)
+
+    def _delete(self):
+        params = {"mat_no": self.ids.mat_no.text}
+        url = urlTo('personal_info')
+        AsyncRequest(url, params=params, method='DELETE', on_success=self.record_deleted, on_failure=self.show_error)
+
+    def show_error(self, resp):
+        try:
+            error = resp.json()
+            if isinstance(error, dict):
+                error = error.get("detail", "")
+        except Exception as e:
+            error = ""
+        ErrorPopup(error or "Record not found")
 
     def show_add_error(self, resp):
         resp = '' if not resp.text else resp.json()
@@ -132,6 +143,10 @@ class PersonalInfo(FormTemplate):
 
     def record_added(self, resp):
         SuccessPopup('Record added')
+        self.clear_fields()
+
+    def record_deleted(self, resp):
+        SuccessPopup('Record deleted')
         self.clear_fields()
 
     def load_image(self, instance, touch):
